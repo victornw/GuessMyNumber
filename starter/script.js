5faa4b1d5ce18f62038dd349a05965c13f4521ee
Victornw <victormarques.dev@gmail.com>
2022-02-25 20:43:52 -0300

    c4 - adding: greater, lower and win cases

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,23 @@
 "use strict";
 
+const random = Math.trunc(Math.random() * 20 + 1);
+console.log(random);
 document.querySelector(".check").addEventListener("click", function () {
   let userNumber = Number(document.querySelector(".guess").value);
-  console.log(typeof userNumber);
+  console.log(userNumber);
 
   if (!userNumber) {
     document.querySelector(".message").textContent = "Please type a number.";
+  } else if (userNumber > random) {
+    document.querySelector(
+      ".message"
+    ).textContent = `⏬ The SECRET NUMBER is LOWER than ${userNumber} `;
+  } else if (userNumber < random) {
+    document.querySelector(
+      ".message"
+    ).textContent = `⏫ The SECRET NUMBER is GREATER than ${userNumber}`;
+  } else if (userNumber === random) {
+    document.querySelector(".message").textContent = "You Win! 🥳";
+    document.querySelector(".number").textContent = random;
   }
 });
