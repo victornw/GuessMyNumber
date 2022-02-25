37468433aefce15776aee9a44080755c6a005ac9
Victornw <victormarques.dev@gmail.com>
2022-02-25 20:16:25 -0300

    c3 - Setting up the 'empty number' case

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,10 @@
-'use strict';
+"use strict";
+
+document.querySelector(".check").addEventListener("click", function () {
+  let userNumber = Number(document.querySelector(".guess").value);
+  console.log(typeof userNumber);
+
+  if (!userNumber) {
+    document.querySelector(".message").textContent = "Please type a number.";
+  }
+});
