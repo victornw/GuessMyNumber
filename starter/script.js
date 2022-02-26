a788fbb4eac2988c39df201e20e3bb2e465927e7
Victornw <victormarques.dev@gmail.com>
2022-02-25 21:08:27 -0300

    c5 - adding: highscore and RESET button

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const random = Math.trunc(Math.random() * 20 + 1);
+let random = Math.trunc(Math.random() * 20 + 1);
 console.log(random);
 document.querySelector(".check").addEventListener("click", function () {
   let userNumber = Number(document.querySelector(".guess").value);
@@ -12,12 +12,38 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(
       ".message"
     ).textContent = `⏬ The SECRET NUMBER is LOWER than ${userNumber} `;
+    document.querySelector(".score").textContent -= 1;
   } else if (userNumber < random) {
     document.querySelector(
       ".message"
     ).textContent = `⏫ The SECRET NUMBER is GREATER than ${userNumber}`;
+    document.querySelector(".score").textContent -= 1;
   } else if (userNumber === random) {
     document.querySelector(".message").textContent = "You Win! 🥳";
     document.querySelector(".number").textContent = random;
+    if (
+      Number(document.querySelector(".score").textContent) >
+      Number(document.querySelector(".highscore").textContent)
+    ) {
+      document.querySelector(".highscore").textContent = Number(
+        document.querySelector(".score").textContent
+      );
+    }
   }
 });
+
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = 20;
+  document.querySelector(".guess").value = "";
+  random = Math.trunc(Math.random() * 20 + 1);
+});
+
+// if (
+//   Number(document.querySelector(".score").textContent) >
+//   Number(document.querySelector(".highscore").textContent)
+// ) {
+//   document.querySelector(".highscore").textContent = Number(
+//     document.querySelector(".score").textContent
+//   );
+// }
