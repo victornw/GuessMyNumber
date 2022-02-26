13b7eb8818c02f963a5c4d9f60b149742caa2bb9
Victornw <victormarques.dev@gmail.com>
2022-02-25 23:06:39 -0300

    c6 - adding: 'lose' condition

--- starter/script.js
+++ starter/script.js
@@ -9,25 +9,24 @@ document.querySelector(".check").addEventListener("click", function () {
   if (!userNumber) {
     document.querySelector(".message").textContent = "Please type a number.";
   } else if (userNumber > random) {
-    document.querySelector(
-      ".message"
-    ).textContent = `⏬ The SECRET NUMBER is LOWER than ${userNumber} `;
-    document.querySelector(".score").textContent -= 1;
+    if (Number(document.querySelector(".score").textContent) > 0) {
+      document.querySelector(".message").textContent = `⏬ The SECRET NUMBER is LOWER than ${userNumber} `;
+      document.querySelector(".score").textContent--;
+    } else {
+      document.querySelector(".message").textContent = "YOU LOSE THE GAME! ❌";
+    }
   } else if (userNumber < random) {
-    document.querySelector(
-      ".message"
-    ).textContent = `⏫ The SECRET NUMBER is GREATER than ${userNumber}`;
-    document.querySelector(".score").textContent -= 1;
+    if (Number(document.querySelector(".score").textContent) > 0) {
+      document.querySelector(".message").textContent = `⏫ The SECRET NUMBER is GREATER than ${userNumber}`;
+      document.querySelector(".score").textContent--;
+    } else {
+      document.querySelector(".message").textContent = "YOU LOSE THE GAME! ❌";
+    }
   } else if (userNumber === random) {
     document.querySelector(".message").textContent = "You Win! 🥳";
     document.querySelector(".number").textContent = random;
-    if (
-      Number(document.querySelector(".score").textContent) >
-      Number(document.querySelector(".highscore").textContent)
-    ) {
-      document.querySelector(".highscore").textContent = Number(
-        document.querySelector(".score").textContent
-      );
+    if (Number(document.querySelector(".score").textContent) > Number(document.querySelector(".highscore").textContent)) {
+      document.querySelector(".highscore").textContent = (document.querySelector(".score").textContent - 20) * -1 + 1;
     }
   }
 });
@@ -38,12 +37,3 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector(".guess").value = "";
   random = Math.trunc(Math.random() * 20 + 1);
 });
-
-// if (
-//   Number(document.querySelector(".score").textContent) >
-//   Number(document.querySelector(".highscore").textContent)
-// ) {
-//   document.querySelector(".highscore").textContent = Number(
-//     document.querySelector(".score").textContent
-//   );
-// }
